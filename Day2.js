f21dc3bdb7f2967cbbd4317259e43a17be056d9b
Suriya-pradath <108716544+Suriya-pradath@users.noreply.github.com>
2023-03-31 14:17:59 +0530

    Add files via upload

---
+++ Day2.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Greetings(props) {
+  return (
+    <div>
+    <h2>
+    <p align="center">{props.name}</p>
+    </h2>
+    </div>
+  )
+}
+export default Greetings;
